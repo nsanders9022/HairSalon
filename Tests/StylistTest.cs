2761eb91dc14956fe6a321867845fe7e3ce6e03c
Nicole Sanders <nsanders9022@gmail.com>
2017-02-24 08:48:15 -0800

    add tests for GetAll and Save methods

--- Tests/StylistTest.cs
+++ Tests/StylistTest.cs
@@ -29,6 +29,35 @@ namespace HairSalonApp
             }
         }
 
+        [Fact]
+        public void Save_TestIfSaved_saved()
+        {
+            //Arrange
+
+            Stylist stylist1 = new Stylist("Sally", 5);
+            stylist1.Save();
+
+            List<Stylist> testStylistList = new List<Stylist> {stylist1};
+            List<Stylist> resultStylistList = Stylist.GetAll();
+
+            //Assert
+            Assert.Equal(testStylistList, resultStylistList);
+        }
+
+        [Fact]
+        public void GetAll_ReturnAllStylists_list()
+        {
+            Stylist stylist1 = new Stylist("Hector" 8);
+            Stylist stylist2 = new Stylist("Nancy", 1);
+            stylist1.Save();
+            stylist2.Save();
+
+            List<Stylist> testStylistList = new List<Stylist> {stylist1, stylist2};
+            List<Stylist> resultStylistList = Stylist.GetAll();
+            
+            Assert.Equal(testStylistList, resultStylistList);
+        }
+
         public void Dispose()
         {
             Stylist.DeleteAll();
